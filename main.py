942e5d0768fec02f24bd1bfba83cd0511c5427fc
Alex <alex@matica.com>
2023-03-19 15:49:40 +0000

    working for frequency generator

--- main.py
+++ main.py
@@ -3,15 +3,28 @@ from gurgleapps_webserver import GurgleAppsWebserver
 import utime as time
 import uasyncio as asyncio
 
-async def set_frequency(writer, freq):
-    global frequency
-    frequency = freq
+async def send_status(writer):
     writer.write('HTTP/1.0 200 OK\r\nContent-type: application/json\r\n\r\n')
     # send boolean status and number frequency
     response = "{status: " + str(status) + ", freq: " + str(frequency) + "}"
     writer.write(response)
     await writer.drain()
     await writer.wait_closed()
+
+async def set_frequency(writer, freq):
+    global frequency
+    frequency = freq
+    await send_status(writer)
+
+async def stop_generating(writer):
+    global status
+    status = False
+    await send_status(writer)
+
+async def start_generating(writer):
+    global status
+    status = True
+    await send_status(writer)
 
 async def main():
     await server.start_server()
@@ -26,6 +39,8 @@ async def run():
 
 server = GurgleAppsWebserver(config.WIFI_SSID, config.WIFI_PASSWORD)
 server.add_function_route("^/set-frequency/(\d+)$", set_frequency)
+server.add_function_route("^/stop$", stop_generating)
+server.add_function_route("^/start$", start_generating)
 
 frequency = 20
 status = True
