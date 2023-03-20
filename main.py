597643a66051ab4b4d8c867fa30722919fc0fc85
Alex <alex@matica.com>
2023-03-20 16:39:49 +0000

    flashing led

--- main.py
+++ main.py
@@ -2,26 +2,33 @@ import config
 from gurgleapps_webserver import GurgleAppsWebserver
 import utime as time
 import uasyncio as asyncio
+from machine import Pin
+import ujson as json
+
+delay = 0.5
+status = True
+led = Pin("LED", Pin.OUT)
 
 async def send_status(writer):
     writer.write('HTTP/1.0 200 OK\r\nContent-type: application/json\r\n\r\n')
     # send boolean status and number frequency
-    response = "{status: " + str(status) + ", freq: " + str(frequency) + "}"
+    response = json.dumps({"status": status, "delay": delay})
     writer.write(response)
     await writer.drain()
     await writer.wait_closed()
 
-async def set_frequency(writer, freq):
-    global frequency
-    frequency = freq
+async def set_delay(writer, new_delay):
+    print("new delay: " + new_delay)
+    global delay
+    delay = float(new_delay)
     await send_status(writer)
 
-async def stop_generating(writer):
+async def stop_flashing(writer):
     global status
     status = False
     await send_status(writer)
 
-async def start_generating(writer):
+async def start_flashing(writer):
     global status
     status = True
     await send_status(writer)
@@ -30,19 +37,21 @@ async def main():
     await server.start_server()
 
 async def background_task():
+    global delay, status
     while True:
-        await asyncio.sleep(10)
-        print("main")
+        await asyncio.sleep(delay)
+        if status:
+            led.toggle()
+        else:
+            led.off()
 
 async def run():
     await asyncio.gather(main(), background_task())
 
 server = GurgleAppsWebserver(config.WIFI_SSID, config.WIFI_PASSWORD)
-server.add_function_route("^/set-frequency/(\d+)$", set_frequency)
-server.add_function_route("^/stop$", stop_generating)
-server.add_function_route("^/start$", start_generating)
-
-frequency = 20
-status = True
+server.add_function_route("^/set-delay/(\d+(?:\.\d+)?)$", set_delay)
+server.add_function_route("^/stop$", stop_flashing)
+server.add_function_route("^/start$", start_flashing)
+server.add_function_route("^/status$", send_status)
 
 asyncio.run(run())
